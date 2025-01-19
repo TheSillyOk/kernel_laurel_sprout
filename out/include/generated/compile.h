9de0c08a0845e5f80e1b482c0a8f8e1101882881
Ok <xiaomi.dilation997@passinbox.com>
2025-01-18 22:00:02 -0300

    Change build.sh & Cherry pick [5a4e64e](https://github.com/Mi-A3-laurel-sprout/kernel_xiaomi_XD/commit/5a4e64ecbcc288d0628b09ae0adc3c81daf2a1ec)

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 10 */
+/* This file is auto generated, version 11 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#10 SMP PREEMPT Sat Jan 18 19:50:24 -03 2025"
+#define UTS_VERSION "#11 SMP PREEMPT Sat Jan 18 21:46:09 -03 2025"
 #define LINUX_COMPILE_BY "( ͡❛ ͜ʖ ͡❛)✌"
 #define LINUX_COMPILE_HOST "🇺🇦"
 #define LINUX_COMPILER "clang version 18.1.8, LLD 18.1.8"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.14.355-openela-V9-g9a30fd2869fd"
+#define UTS_RELEASE "4.14.336-V9-g33f9a77abdfb-dirty"
