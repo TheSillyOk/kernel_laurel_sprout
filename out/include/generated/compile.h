7e6b6aedf1d3aadf58a6a5b746569de82aa7a8ff
Ok <xiaomi.dilation997@passinbox.com>
2025-01-21 21:34:59 -0300

    Automatically added KSU Next stuff

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 12 */
+/* This file is auto generated, version 14 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#12 SMP PREEMPT Tue Jan 21 20:15:39 -03 2025"
+#define UTS_VERSION "#14 SMP PREEMPT Tue Jan 21 21:32:47 -03 2025"
 #define LINUX_COMPILE_BY "( ͡❛ ͜ʖ ͡❛)✌"
 #define LINUX_COMPILE_HOST "🇺🇦"
 #define LINUX_COMPILER "clang version 18.1.8, LLD 18.1.8"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.14.336-V9-KSUNext-g7294afecd7f2-dirty"
+#define UTS_RELEASE "4.14.336-V9-KSUNext-g2e7767e16312-dirty"
