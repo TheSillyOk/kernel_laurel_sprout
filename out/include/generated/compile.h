b31d2936a2d33f620d70290a168453edb497c012
Ok <xiaomi.dilation997@passinbox.com>
2025-02-02 20:22:06 -0300

    Stuff

--- out/include/generated/compile.h
+++ out/include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 15 */
+/* This file is auto generated, version 16 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "aarch64"
-#define UTS_VERSION "#15 SMP PREEMPT Sun Feb 2 19:22:46 -03 2025"
+#define UTS_VERSION "#16 SMP PREEMPT Sun Feb 2 20:20:08 -03 2025"
 #define LINUX_COMPILE_BY "( ͡❛ ͜ʖ ͡❛)✌"
 #define LINUX_COMPILE_HOST "🇺🇦"
 #define LINUX_COMPILER "clang version 19.1.7, LLD 19.1.7"
--- out/include/generated/utsrelease.h
+++ out/include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "4.14.355-openela-V9-Ok-gfad4a2090cc6"
+#define UTS_RELEASE "4.14.355-openela-V9-Ok-g04d766f15102"
